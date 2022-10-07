2fecdbd738fc86e0208bc46353657e129a4cb164
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2022-10-07 09:24:17 +0530

    n=int(input()) l=list(map(int,input().split())) s=0 for i in range(len(l)):     s+=l[i] a=s/n c=0 for i in range(0,len(l)):     if l[i]<=a:         c+=1 print(c)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+for i in range(len(l)):
+    s+=l[i]
+a=s/n
+c=0
+for i in range(0,len(l)):
+    if l[i]<=a:
+        c+=1
+print(c)
+    
+
+    
