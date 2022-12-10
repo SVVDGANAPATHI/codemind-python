407dbc6d250c0a3dc8a2f36f2e97bb825bdad436
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2022-12-10 12:04:38 +0530

    def isAutomorphic(N) :
        # Store the square
        sq = N * N
          
        # Start Comparing digits
        while (N > 0) :
     
            # Return false, if any digit of N doesn't
            # match with its square's digits from last
            if (N % 10 != sq % 10) :
                return False
       
            # Reduce N and square
            N //= 10
            sq //= 10
       
        return True
       
    # Driver code
    N=int(input())
    if isAutomorphic(N) :
        print ("Automorphic Number")
    else :
        print  ("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,11 +1,25 @@
-n=int(input())
-k=n
-m=n*n
-c=0
-while k:
-    k=k//10
-    c+=1
-if m%10**c==n:
-    print("Automorphic Number")
-else:
-    print("Not an Automorphic Number")
+def isAutomorphic(N) :
+ 
+    # Store the square
+    sq = N * N
+      
+    # Start Comparing digits
+    while (N > 0) :
+ 
+        # Return false, if any digit of N doesn't
+        # match with its square's digits from last
+        if (N % 10 != sq % 10) :
+            return False
+   
+        # Reduce N and square
+        N //= 10
+        sq //= 10
+   
+    return True
+   
+# Driver code
+N=int(input())
+if isAutomorphic(N) :
+    print ("Automorphic Number")
+else :
+    print  ("Not an Automorphic Number")
