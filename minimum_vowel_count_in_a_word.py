e0a10fa722fa5acb72619423c2d0bfec8fdcbe47
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-23 10:56:49 +0530

    l=list(input().split()) def cou(x):     l1=['a','e','i','o','u']     c=0     for i in x:         if i.lower() in l1:             c+=1     return c k=[] for i in l:     k.append(cou(i)) print(min(k))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+l=list(input().split())
+def cou(x):
+    l1=['a','e','i','o','u']
+    c=0
+    for i in x:
+        if i.lower() in l1:
+            c+=1
+    return c
+k=[]
+for i in l:
+    k.append(cou(i))
+print(min(k))
+    
+            
