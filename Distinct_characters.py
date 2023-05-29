39b40c56a2c645a4d768fdcb399862bf41a49ccf
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-29 12:27:07 +0530

    n=input().lower() n=n.replace(" ",'') l=[] d={} for i in n:     if i not in d:         d[i]=1         l.append(i)     else:         d[i]+=1 p=[k for k,v in d.items() if v==1] print("".join(sorted(p)))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,6 +1,12 @@
 n=input().lower()
-l=n.replace(" ",'')
-l=sorted(set(l))
-k=''.join(l)
-print(k)
-
+n=n.replace(" ",'')
+l=[]
+d={}
+for i in n:
+    if i not in d:
+        d[i]=1
+        l.append(i)
+    else:
+        d[i]+=1
+p=[k for k,v in d.items() if v==1]
+print("".join(sorted(p)))
