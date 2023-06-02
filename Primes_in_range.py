57abf9c2ff1d93deff6a9b1e97884b2f08bc80c5
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-02 20:46:00 +0530

    import math n=int(input()) m=int(input()) def p(x):     if x==1:         return False     for i in range(2,int(math.sqrt(x))+1):         if x%i==0:             return False     return True c=0 for i in range(n,m+1):     if p(i):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,14 +1,15 @@
 import math
 n=int(input())
 m=int(input())
-def pr(x):
+def p(x):
+    if x==1:
+        return False
     for i in range(2,int(math.sqrt(x))+1):
         if x%i==0:
             return False
     return True
 c=0
 for i in range(n,m+1):
-    if pr(i) and i!=1:
+    if p(i):
         c+=1
 print(c)
-    
