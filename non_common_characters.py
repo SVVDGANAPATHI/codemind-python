1a6bca58ea38fbcd4b48cdf2be24220bdb6c48d0
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-02 09:33:56 +0530

    n=input().lower().split() m=input().lower().split() l=[] k=[] for i in n:     for j in i:        if j not in l:           l.append(j) for i in m:     for j in i:        if j not in k:           k.append(j) p=set(l) o=set(k) t=p.symmetric_difference(o) t=list(t) t.sort() print("".join(t))

---
+++ non_common_characters.py
@@ -0,0 +1,18 @@
+n=input().lower().split()
+m=input().lower().split()
+l=[]
+k=[]
+for i in n:
+    for j in i: 
+       if j not in l:
+          l.append(j)
+for i in m:
+    for j in i: 
+       if j not in k:
+          k.append(j)
+p=set(l)
+o=set(k)
+t=p.symmetric_difference(o)
+t=list(t)
+t.sort()
+print("".join(t))
