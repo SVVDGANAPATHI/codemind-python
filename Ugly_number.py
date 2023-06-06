99af136866d2c50cc5754b8b04fb02965eeb4f5d
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-06 14:16:32 +0530

    n=int(input()) def p(x):     for i in range(2,n):         if x%i==0:             return False     return True if n>0:     l=[]     for i in range(6,n+1):         if p(i) and n%i==0:             l.append(i)             break     if len(l)!=0:         print("Not Ugly Number")     else:         print("Ugly Number") else:     print("Not Ugly Number")

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,21 +1,18 @@
-import math
 n=int(input())
 def p(x):
-    if x==1:
-        return False
-    for i in range(2,int(math.sqrt(x)+1)):
+    for i in range(2,n):
         if x%i==0:
             return False
     return True
-if n<0:
-    print("Not Ugly Number")
-else:
-    f=0
+if n>0:
+    l=[]
     for i in range(6,n+1):
-        if p(i)==True and n%i==0:
-            f=1
-            print("Not Ugly Number")
+        if p(i) and n%i==0:
+            l.append(i)
             break
-    if f==0:
+    if len(l)!=0:
+        print("Not Ugly Number")
+    else:
         print("Ugly Number")
-    
+else:
+    print("Not Ugly Number")
