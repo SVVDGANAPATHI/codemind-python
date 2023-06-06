73f9f11eaefca1e7a5512672cd468e477bc044b4
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-06 19:42:45 +0530

    n=input() l=[] for i in range(2,int(n)):     if str(i)==str(i)[::-1]:         l.append(int(i)) for i in range(int(n)+1,int(n)**2):     if str(i)==str(i)[::-1]:         l.append(int(i))         break ma=l[-1] mi=l[-2] if ma-int(n)==int(n)-mi:     print(mi,ma) elif ma-int(n) < int(n)-mi:     print(ma) else:     print(mi)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,19 +1,17 @@
-n=int(input())
+n=input()
 l=[]
-for i in range(n):
-    i=str(i)
-    if i==i[::-1]:
+for i in range(2,int(n)):
+    if str(i)==str(i)[::-1]:
         l.append(int(i))
-for i in range(n+1,n**2):
-    i=str(i)
-    if i==i[::-1]:
+for i in range(int(n)+1,int(n)**2):
+    if str(i)==str(i)[::-1]:
         l.append(int(i))
         break
 ma=l[-1]
 mi=l[-2]
-if ma-n == n-mi:
+if ma-int(n)==int(n)-mi:
     print(mi,ma)
-elif ma-n < n-mi:
+elif ma-int(n) < int(n)-mi:
     print(ma)
 else:
     print(mi)
