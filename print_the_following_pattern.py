74e437203a00425b720e9fa37c639e41b9070717
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-06 10:34:36 +0530

    n=int(input()) p=64+n o=n+1 for i in range(n):     for j in range(o-1):         print(chr(p),end=" ")     p-=1     o=o-1     print('')

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,9 @@
 n=int(input())
-p=65
+p=64+n
+o=n+1
 for i in range(n):
-    for j in range(n):
+    for j in range(o-1):
         print(chr(p),end=" ")
-    p+=1
-    print("")
+    p-=1
+    o=o-1
+    print('')
